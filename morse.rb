cfa01634a5055b39c7cd8f26378c96bf4d4a12b8
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-09-27 18:36:41 -0500

    Fix linters errors

--- morse.rb
+++ morse.rb
@@ -1,28 +1,28 @@
-def decode_char(morseChar)
+def decode_char(morse_char)
   morse_hash = {
-    "a" => ".-", "b" => "-...", "c" => "-.-.", "d" => "-..", "e" => ".", "f" => "..-.", "g" => "--.", "h" => "....",
-    "i" => "..", "j" => ".---", "k" => "-.-", "l" => ".-..", "m" => "--", "n" => "-.", "o" => "---", "p" => ".--.",
-    "q" => "--.-", "r" => ".-.", "s" => "...", "t" => "-", "u" => "..-", "v" => "...-", "w" => ".--", "x" => "-..-",
-    "y" => "-.--", "z" => "--..", " " => " ", "1" => ".----", "2" => "..---", "3" => "...--", "4" => "....-",
-    "5" => ".....", "6" => "-....", "7" => "--...", "8" => "---..", "9" => "----.", "0" => "-----"
+    'a' => '.-', 'b' => '-...', 'c' => '-.-.', 'd' => '-..', 'e' => '.', 'f' => '..-.', 'g' => '--.', 'h' => '....',
+    'i' => '..', 'j' => '.---', 'k' => '-.-', 'l' => '.-..', 'm' => '--', 'n' => '-.', 'o' => '---', 'p' => '.--.',
+    'q' => '--.-', 'r' => '.-.', 's' => '...', 't' => '-', 'u' => '..-', 'v' => '...-', 'w' => '.--', 'x' => '-..-',
+    'y' => '-.--', 'z' => '--..', ' ' => ' ', '1' => '.----', '2' => '..---', '3' => '...--', '4' => '....-',
+    '5' => '.....', '6' => '-....', '7' => '--...', '8' => '---..', '9' => '----.', '0' => '-----'
   }
-  return morse_hash.key(morseChar).upcase
+  morse_hash.key(morse_char).upcase
 end
 
 def decode_word(word)
-  morseWord = ''
-  word.split(" ").each do |i|
-    morseWord.concat(decode_char i)
+  morse_word = ''
+  word.split.each do |i|
+    morse_word.concat(decode_char(i))
   end
-  return morseWord
+  morse_word
 end
 
 def decode(message)
-  final_message = ""
-  message.split("   ").each do |i|
-    final_message.concat(decode_word i).concat(" ")
+  final_message = ''
+  message.split('   ').each do |i|
+    final_message.concat(decode_word(i)).concat(' ')
   end
-  return final_message
+  final_message
 end
 
-puts decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+puts decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
