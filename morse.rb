6f063842c27009a0714f8cd3ff3b708eec675f4a
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-09-27 17:52:55 -0500

    Create the method to decode the whole message

--- morse.rb
+++ morse.rb
@@ -2,7 +2,7 @@ def decode_char(morseChar)
     morse_hash = {
         "a" => ".-","b" => "-...","c" => "-.-.","d" => "-..","e" => ".","f" => "..-.","g" => "--.","h" => "....","i" => "..","j" => ".---","k" => "-.-","l" => ".-..","m" => "--","n" => "-.","o" => "---","p" => ".--.","q" => "--.-","r" => ".-.","s" => "...","t" => "-","u" => "..-","v" => "...-","w" => ".--","x" => "-..-","y" => "-.--","z" => "--.."," " => " ","1" => ".----","2" => "..---","3" => "...--","4" => "....-","5" => ".....","6" => "-....","7" => "--...","8" => "---..","9" => "----.","0" => "-----"
     }
-    return morse_hash.key(morseChar)
+    return morse_hash.key(morseChar).upcase
 end
 
 def decode_word(word)
@@ -12,3 +12,13 @@ def decode_word(word)
     end
     return morseWord
 end
+
+def decode(message)
+    final_message = ""
+    message.split("   ").each do |i|
+        final_message.concat(decode_word i).concat(" ")
+    end
+    return final_message
+end    
+
+puts decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
